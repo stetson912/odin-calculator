d7b8a6f4e11db76ebbd1e81ca2cec456b3f4a9be
stetson912 <trenton.stetson@gmail.com>
2022-08-08 21:21:10 -0700

    Finish calculator logic.

--- script.js
+++ script.js
@@ -13,23 +13,160 @@ function multiply(a,b){
 }
 
 function divide(a,b){
-    return a/b
+    return (a/b).toFixed(1)
 }
 
 function operate(operator,a,b){
-    switch(operator){
+    switch (operator){
         case '+':
             return add(a,b)
-        case '-': 
+        case'-':
             return subtract(a,b)
+        case '/':
+            if(b>0){
+                return divide(a,b)
+            }
+            return 'Error: can\'t divide by 0'
         case '*':
             return multiply(a,b)
-        case '/':
-            return divide(a,b)
-        default:
-            return "Invalid Operation Occured"
+        default: 
+            console.log(result)
     }
 }
+
+// helper functions
+function setNumber(e){
+    let val = e.target.innerText
+    if(currentNum){
+        if(currentNum.includes('.') && val == '.'){
+            return
+        }
+    }
+    if(display.innerText === 0 || display.innerText === "0"){
+        display.innerText = val
+        currentNum = val
+        return
+    }
+    if(operator && !currentNum){
+        display.innerText = val
+        currentNum = val
+        return
+    }
+    display.innerText += val
+    currentNum += val
+}
+
+function updateDisplay(e){
+    let val = e.target.innerText
+    switch(val){
+        case 'Clear':
+            prevNum = null
+            currentNum = null
+            operator = undefined
+            result = null
+
+            display.innerText = 0
+            break
+        case '+':
+            if(!prevNum){
+                prevNum = currentNum
+                currentNum = null
+                operator = '+'
+                break
+            }
+            if(prevNum && currentNum && operator){
+                result = operate(operator,parseFloat(prevNum),parseFloat(currentNum))
+                prevNum = result
+                currentNum = null
+                operator = '+'
+                display.innerText = result
+            }
+        case '-':
+            if(!prevNum){
+                prevNum = currentNum
+                currentNum = null
+                operator = '-'
+                break
+            }
+            if(prevNum && currentNum && operator){
+                result = operate(operator,parseFloat(prevNum),parseFloat(currentNum))
+                prevNum = result
+                currentNum = null
+                operator = '-'
+                display.innerText = result
+            }
+        case '*':
+            if(!prevNum){
+                prevNum = currentNum
+                currentNum = null
+                operator = '*'
+                break
+            }
+            if(prevNum && currentNum && operator){
+                result = operate(operator,parseFloat(prevNum),parseFloat(currentNum))
+                prevNum = result
+                currentNum = null
+                operator = '*'
+                display.innerText = result
+            }
+        case '/':
+            if(!prevNum){
+                prevNum = currentNum
+                currentNum = null
+                operator = '/'
+                break
+            }
+            if(prevNum && currentNum && operator){
+                result = operate(operator,parseFloat(prevNum),parseFloat(currentNum))
+                prevNum = result
+                currentNum = null
+                operator = '/'
+                display.innerText = result
+            }
+        case '=':
+            if(prevNum && currentNum && operator){
+                result = operate(operator,parseFloat(prevNum),parseFloat(currentNum))
+                prevNum = null
+                currentNum = result
+                display.innerText = result
+                break
+            }
+        default:
+            console.log(prevNum,currentNum,operator)
+            break
+            
+            
+    }
+    
+}
+
+
+// main logic
+// elements
+let numberButtons = [...document.querySelectorAll('.number')]
+let operatorButtons = [...document.querySelectorAll('.operator')]
+let display = document.querySelector('.display')
+display.innerText = 0
+
+let prevNum = null
+let currentNum = null
+let operator = null
+// let operator2 = null
+let result = null
+
+
+//map through buttons and add event listeners to update display
+numberButtons.map(btn=>{
+    btn.addEventListener('click',(e)=>{
+        setNumber(e)
+    })
+})
+
+operatorButtons.map(btn=>{
+    btn.addEventListener('click',(e)=>{
+        updateDisplay(e)
+    })
+})
 // console.log(add(1,2))
 // console.log(multiply(1,2))
 // console.log(divide(1,2))
